39226932c9420ac15304155d40b74fd639dc607e
Melanga Kasun <46860391+MelangaK97@users.noreply.github.com>
2019-11-07 18:57:32 +0530

    Update activity.php

--- application/views/pages/trainee/activity.php
+++ application/views/pages/trainee/activity.php
@@ -1,5 +1,32 @@
-<div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
-    <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
-</div>
+<br><br><form>
+    <fieldset>
+        <legend class="font-weight-bold">Training</legend><br>
+        <fieldset class="form-group">
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3" selected="">Didn't completed the training.
+                </label>
+            </div>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">Partially completed the training.
+                </label>
+            </div>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">Successfully complete the training.
+                </label>
+            </div>
+        </fieldset>
+        <div class="form-group">
+            <label for="exampleInputFile">Upload your video here...</label>
+            <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleTextarea">Share your experiences with us</label>
+            <textarea class="form-control" id="exampleTextarea" rows="3" name="editor1"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit Details</button>
+        <button type="button" class="btn btn-secondary btn-sm">Cancel</button>
+    </fieldset>
+</form>
